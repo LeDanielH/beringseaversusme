83857ae6918f9f0b9d31976d798daa018a559245
Daniel Zeman <ledaniel.zeman@gmail.com>
2019-02-17 00:58:39 +0100

    cleanup

--- src/components/footer/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = ({ email }) => (
-	<footer>
-		<p>{email}</p>
-	</footer>
-)
-
-export default Footer
--- src/components/tooltip/tooltip-helper/tooltip-helper.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const TooltipHelper = ({ content }) => (
-	<div className="s-helper__tooltip-content">{content}</div>
-)
-
-export default TooltipHelper
